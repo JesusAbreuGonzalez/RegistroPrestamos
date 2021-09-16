7ae65248f050f8c7c5a640a6f25ef8f00dcfd05e
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-09-16 19:18:40 -0400

    Creando entidad Personas

---
+++ RegistroPrestamos/Models/Personas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el teléfono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la cédula")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la dirección")]
+        public string Direccion { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+
+        public Personas()
+        {
+            PersonaId = 0;
+            Nombre = string.Empty;
+            Telefono = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            FechaNacimiento = DateTime.Now;
+        }
+    }
+}
