1092c078ea240b5ff71cd6f719ee97018fde432e
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-09-16 19:52:19 -0400

    Agregando relaciones

---
+++ RegistroPrestamos/Models/Prestamos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.Models
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el concepto")]
+        public string Concepto { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el monto")]
+        public double Monto { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el balance")]
+        public double Balance { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        public Prestamos()
+        {
+            PrestamoId = 0;
+            Concepto = string.Empty;
+            Monto = 0;
+            Balance = 0;
+            FechaCreacion = DateTime.Now;
+            PersonaId = new List<Personas>();
+        }
+
+        [ForeignKey("PrestamosId")]
+        public virtual List<Personas> PersonaId { get; set; }
+
+    }
+}
