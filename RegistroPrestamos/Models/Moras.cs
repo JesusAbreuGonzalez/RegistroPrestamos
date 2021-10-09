b588b8ca919b56010be6c8b439f8e8b2785a16d0
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-08 21:43:20 -0400

    Cambiando nombre de foreign key morasDetalles a moraDetalle

--- RegistroPrestamos/Models/Moras.cs
+++ RegistroPrestamos/Models/Moras.cs
@@ -13,14 +13,14 @@ namespace RegistroPrestamos.Models
         public DateTime Fecha { get; set; }
         public double Total { get; set; }
 
-        public virtual ICollection<MorasDetalle> morasDetalles { get; set; }
+        public virtual ICollection<MorasDetalle> morasDetalle { get; set; }
 
         Moras()
         {
             MoraId = 0;
             Fecha = DateTime.Now;
             Total = 0;
-            morasDetalles = new HashSet<MorasDetalle>();
+            morasDetalle = new HashSet<MorasDetalle>();
         }
     }
 }
