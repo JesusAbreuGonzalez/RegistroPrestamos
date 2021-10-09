d42e0e7ab0bea7c0e44a80687f30f7eacabc2b2e
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-08 21:42:23 -0400

    Creando Modelos Moras y MorasDetalle

---
+++ RegistroPrestamos/Models/Moras.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.Models
+{
+    public class Moras
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Total { get; set; }
+
+        public virtual ICollection<MorasDetalle> morasDetalles { get; set; }
+
+        Moras()
+        {
+            MoraId = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            morasDetalles = new HashSet<MorasDetalle>();
+        }
+    }
+}
---
+++ RegistroPrestamos/Models/morasDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.Models
+{
+    public class MorasDetalle
+    {
+        [Key]
+        public int MoraDetalleId { get; set; }
+        public int MoraId { get; set; }
+        public int PrestamoId { get; set; }
+        public double Valor { get; set; }
+
+        MorasDetalle()
+        {
+            MoraDetalleId = 0;
+            MoraId = 0;
+            PrestamoId = 0;
+            Valor = 0;
+        }
+    }
+}
